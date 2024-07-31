4e7bbdca6aabac8cd51a427234fc3a000c07eb54
Sortaxz <kurtberkay944@gmail.com>
2024-07-31 10:20:13 +0300

    ui sahnedeki ui objeleri bulundu ve oyuncu adi yazdirildi

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,12 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
-{
+{   
+    [SerializeField] private TMP_InputField KullaniciAdi_InputField;
+    [SerializeField] private Button OyunuBaslat_Button;
+
+    [SerializeField] private Slider HealtBar;
+    [SerializeField] private TextMeshProUGUI oyuncuAdiText;
+    [SerializeField] private TextMeshProUGUI healtText;
+
+    bool find = false;
+    private void Awake() 
+    {
+        DontDestroyOnLoad(gameObject);    
+    }
+    private void Start() 
+    {
+    }
+
     public void Basla_Button()
     {
-        SceneManager.LoadScene(1);
+        if(KullaniciAdi_InputField.text != "")
+        {
+            string oyuncuAdi = KullaniciAdi_InputField.text;
+            PlayerPrefs.SetString("oyuncuAdi", oyuncuAdi);
+            
+            SceneManager.LoadScene(1);
+            StartCoroutine(FindUIElements());
+        }
     }
+
+    private void Update() 
+    {
+        
+    }
+    //HealtBar
+    //OyuncuAdiText
+    private IEnumerator FindUIElements()
+    {
+        print("FindHealtBar");
+        while (true && !find)
+        {
+            yield return new WaitForSeconds(.1f);
+            if(HealtBar == null && oyuncuAdiText == null && healtText == null)
+            {
+                HealtBar = GameObject.FindWithTag("HealtBar").GetComponent<Slider>();
+                oyuncuAdiText = GameObject.FindWithTag("OyuncuAdiText").GetComponent<TextMeshProUGUI>();
+                oyuncuAdiText.text = PlayerPrefs.GetString("oyuncuAdi");
+                healtText = GameObject.FindWithTag("CanDegeriText").GetComponent<TextMeshProUGUI>();
+            }
+            else if(HealtBar != null && oyuncuAdiText != null && healtText != null)
+            {
+                print("buldu");
+                find = true;
+                StopCoroutine(FindUIElements());
+            }
+        }
+        
+    }
+
+    
 }
